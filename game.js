1444ea6e1840919237d276b117e079517fb51d3d
ryan2c7 <ryan2c7@mail.uc.edu>
2023-07-25 18:03:10 -0400

    visual improvement update

--- game.js
+++ game.js
@@ -1,27 +1,31 @@
 class mainScene {
   preload() {
-    this.load.image('player', 'assets/images/bigmanallen.png');
-    this.load.image('ball', 'assets/images/ball.png');
-    this.load.image('goldball', 'assets/images/goldball.png');
+    this.load.image(`background`, `assets/images/background.png`);
+    this.load.image(`player`, `assets/images/bigmanallen.png`);
+    this.load.image(`ball`, `assets/images/ball.png`);
+    this.load.image(`goldball`, `assets/images/goldball.png`);
   }
   create() {
-    this.player = this.physics.add.sprite(100, 100, 'player');
+    this.background = this.add.image(0, 0, `background`);
+    this.background.setOrigin(0, 0);
+
+    this.player = this.physics.add.sprite(100, 100, `player`);
     this.player.setCollideWorldBounds(true);
 
     this.balls = this.physics.add.group();
 
     for (let i = 0; i < 3; i++) {
-      let ball = this.balls.create(0, 0, 'ball');
+      let ball = this.balls.create(0, 0, `ball`);
       ball.setCollideWorldBounds(true);
       ball.setBounce(1);
       ball.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200));
     }
 
     this.score = 0;
-    let style = { font: '20px Arial', fill: '#fff' };
-    this.scoreText = this.add.text(20, 20, 'score: ' + this.score, style);
+    let style = { font: `20px Arial`, fill: `#fff` };
+    this.scoreText = this.add.text(20, 20, `score: ` + this.score, style);
 
-    this.timerText = this.add.text(600, 20, '1:00', style);
+    this.timerText = this.add.text(600, 20, `1:00`, style);
     this.initialTime = 60;
     this.timer = this.time.addEvent({
       delay: 1000,
@@ -33,13 +37,13 @@ class mainScene {
     this.arrow = this.input.keyboard.createCursorKeys();
   }
   update() {
-    var _this = this;
+    let _this = this;
 
     this.balls.children.iterate(function (ball) {
       _this.physics.world.wrap(ball, 16);
 
       if (_this.physics.overlap(_this.player, ball)) {
-        if (ball.texture.key === 'goldball') {
+        if (ball.texture.key === `goldball`) {
           _this.collectGoldBall(ball);
         } else {
           if (Phaser.Math.RND.frac() <= 0.1) {
@@ -65,9 +69,9 @@ class mainScene {
 
   updateTimer() {
     this.initialTime--;
-    let minutes = Math.floor(this.initialTime / 60).toString().padStart(2, '0');
-    let seconds = (this.initialTime % 60).toString().padStart(2, '0');
-    this.timerText.setText(minutes + ':' + seconds);
+    let minutes = Math.floor(this.initialTime / 60).toString().padStart(2, `0`);
+    let seconds = (this.initialTime % 60).toString().padStart(2, `0`);
+    this.timerText.setText(minutes + `:` + seconds);
 
     if (this.initialTime <= 0) {
       this.timer.paused = true;
@@ -78,7 +82,7 @@ class mainScene {
   spawnGoldBall() {
     let newX = Phaser.Math.Between(20, 680);
     let newY = Phaser.Math.Between(20, 380);
-    let goldBall = this.balls.create(newX, newY, 'goldball');
+    let goldBall = this.balls.create(newX, newY, `goldball`);
     goldBall.setCollideWorldBounds(true);
     goldBall.setBounce(1);
     goldBall.setVelocity(Phaser.Math.Between(-400, 400), Phaser.Math.Between(-400, 400));
@@ -88,9 +92,9 @@ class mainScene {
     goldBall.disableBody(true, true);
 
     this.score += 300;
-    this.scoreText.setText('score: ' + this.score);
+    this.scoreText.setText(`score: ` + this.score);
 
-    var tween = this.tweens.add({
+    let tween = this.tweens.add({
       targets: this.player,
       duration: 200,
       scaleX: 1.2,
@@ -101,7 +105,7 @@ class mainScene {
     for (let i = 0; i < 3; i++) {
       let newX = Phaser.Math.Between(20, 680);
       let newY = Phaser.Math.Between(20, 380);
-      let newBall = this.balls.create(newX, newY, 'ball');
+      let newBall = this.balls.create(newX, newY, `ball`);
       newBall.setCollideWorldBounds(true);
       newBall.setBounce(1);
       newBall.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200));
@@ -112,9 +116,9 @@ class mainScene {
     ball.disableBody(true, true);
 
     this.score += 10;
-    this.scoreText.setText('score: ' + this.score);
+    this.scoreText.setText(`score: ` + this.score);
 
-    var tween = this.tweens.add({
+    let tween = this.tweens.add({
       targets: player,
       duration: 200,
       scaleX: 1.2,
@@ -124,7 +128,7 @@ class mainScene {
 
     let newX = Phaser.Math.Between(20, 680);
     let newY = Phaser.Math.Between(20, 380);
-    let newBall = this.balls.create(newX, newY, 'ball');
+    let newBall = this.balls.create(newX, newY, `ball`);
     newBall.setCollideWorldBounds(true);
     newBall.setBounce(1);
     newBall.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200));
@@ -132,12 +136,12 @@ class mainScene {
 
   gameOver() {
     this.physics.pause();
-    this.timerText.setText('Game Over');
+    this.timerText.setText(`Game Over`);
     let finalScoreText = this.add.text(
       this.sys.game.config.width / 2,
       this.sys.game.config.height / 2,
-      'Final Score: ' + this.score,
-      { font: '32px Arial', fill: '#fff', align: 'center' }
+      `Final Score: ` + this.score,
+      { font: `32px Arial`, fill: `#fff`, align: `center` }
     );
     finalScoreText.setOrigin(0.5);
   }
@@ -145,10 +149,10 @@ class mainScene {
 
 
 new Phaser.Game({
-  width: 700, // Width of the game in pixels
-  height: 400, // Height of the game in pixels
-  backgroundColor: '#3498db', // The background color (blue)
+  width: 800, // Width of the game in pixels
+  height: 500, // Height of the game in pixels
+  backgroundColor: `#3498db`, // The background color (blue)
   scene: mainScene, // The name of the scene we created
-  physics: { default: 'arcade' }, // The physics engine to use
-  parent: 'game', // Create the game inside the <div id="game"> 
+  physics: { default: `arcade` }, // The physics engine to use
+  parent: `game`, // Create the game inside the <div id="game"> 
 });
