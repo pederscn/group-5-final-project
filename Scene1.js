2fed704975c4098a83be1b2ef4da04bf8e53401c
Christian Pedersen UC <109499405+pederscn@users.noreply.github.com>
2023-07-26 15:17:31 -0400

    Title Screen add and game updates (#2)
    Added the title screen with instructions and background, created a button that links to the game, the game begins and now has a pause and end game button, timer and physics stop and players can choose to continue, tried to figure out a restart option, but didn't work as intended so im coming back to that.
    
    Co-authored-by: christianpedersen22 <cjpede22@gmail.com>

--- Scene1.js
+++ Scene1.js
@@ -4,7 +4,7 @@ class Scene1 extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image(`background`, `assets/images/background.png`);
+    this.load.image(`background`, `assets/images/BouncerSimTitleBackground.png`);
     this.load.image(`player`, `assets/images/bigmanallen.png`);
     this.load.image(`ball`, `assets/images/ball.png`);
     this.load.image(`goldball`, `assets/images/goldball.png`);
@@ -12,6 +12,44 @@ class Scene1 extends Phaser.Scene {
   }
 
   create() {
-    // title screen stuff goes here maybe??
+    // Add background image
+    this.add.image(0, 0, `background`).setOrigin(0, 0);
+
+    // Add instructions text
+    this.add.text(
+      this.game.config.width / 2,
+      this.game.config.height / 2,
+      "Instructions:\n\nUse arrow keys to move.\nAllow all of the adults into the bar, bounce all of the babies out!",
+      {
+        font: "20px Arial",
+        fill: "#ffffff",
+        align: "center"
+      }
+    ).setOrigin(0.5);
+
+    // Add start button
+    const startButton = this.add.text(
+      this.game.config.width / 2,
+      this.game.config.height - 100,
+      "Start Game",
+      {
+        font: "30px Arial",
+        fill: "#ffffff",
+        align: "center"
+      }
+    ).setOrigin(0.5);
+    startButton.setInteractive(); // Enable input on the button
+    startButton.on("pointerdown", () => {
+      this.scene.start("bootGame"); // Replace "playGame" with the scene key for your game's main scene
+    });
+
+    // Add button hover effect
+    startButton.on("pointerover", () => {
+      startButton.setStyle({ fill: "#ff0" });
+    });
+
+    startButton.on("pointerout", () => {
+      startButton.setStyle({ fill: "#ffffff" });
+    });
   }
 }
