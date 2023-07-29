6701a2f0b3be1acdf5f4a02e05ee93e1bd62954e
ryan2c7 <133291697+ryan2c7@users.noreply.github.com>
2023-07-29 18:31:49 -0400

    Deeper offshoot of connor offshoot MERGE TO MAIN (#4)
    * New Branch
    
    * Entrance
    
    * Spawning and Entering
    
    * Player
    
    * So far eh..
    
    * Pretty good
    
    * removed timer added basic reputation
    
    * reputation bar addition, score moved
    
    * rep resolution update
    
    * drunk baby speed changes
    
    * drunks and babys spawn method change
    
    * spawn timing and gameOver update
    
    * fixed pause/resume
    
    * spawnrate change
    
    ---------
    
    Co-authored-by: malottcc <malottcolin@gmail.com>

--- game.js
+++ game.js
@@ -1,188 +1,379 @@
 class MainScene extends Phaser.Scene {
   preload() {
-    this.load.image('background', 'assets/images/background.png');
-    this.load.image('player', 'assets/images/bigmanallen.png');
-    this.load.image('ball', 'assets/images/ball.png');
-    this.load.image('goldball', 'assets/images/goldball.png');
-    this.load.image('invisible wall', 'assets/images/invisible wall.png');
+    this.load.image(`background`, `assets/images/background.png`);
+    this.load.image(`rep0`, `assets/images/rep 0.png`);
+    this.load.image(`rep1`, `assets/images/rep 1.png`);
+    this.load.image(`rep2`, `assets/images/rep 2.png`);
+    this.load.image(`rep3`, `assets/images/rep 3.png`);
+    this.load.image(`rep4`, `assets/images/rep 4.png`);
+    this.load.image(`rep5`, `assets/images/rep 5.png`);
+    this.load.image(`player`, `assets/images/player.png`);
+    this.load.image(`drunk`, `assets/images/DrunkGuy2.png`);
+    this.load.image(`baby`, `assets/images/baby.png`);
+    this.load.image(`entrance`, `assets/images/invisible-wall.png`);
+    this.load.image(`exit`, `assets/images/invisible-wall.png`);
+    this.load.image(`fired`, `assets/images/fired.gif`);
   }
 
+  // create ----------------
+
   create() {
-    this.background = this.add.image(0, 0, 'background');
+    this.background = this.add.image(0, 0, `background`);
     this.background.setOrigin(0, 0);
 
-    this.player = this.physics.add.sprite(400, 350, 'player');
+    this.repImage = this.add.image(120, 21, `rep5`);
+    this.repImage.setScale(0.6);
+    this.repImage.setDepth(10);
+
+    this.player = this.physics.add.sprite(400, 400, `player`);
     this.player.setCollideWorldBounds(true);
 
-    // Invisible wall physics
-    this.walls = this.physics.add.group();
-    this.invisibleWall = this.physics.add.sprite(106, 449, 'invisible wall');
-    this.walls.add(this.invisibleWall);
-    this.invisibleWall2 = this.physics.add.sprite(694, 449, 'invisible wall');
-    this.walls.add(this.invisibleWall2);
+    //entrance wall
+    this.entrance = this.physics.add.sprite(
+      this.sys.game.config.width / 2,
+      this.sys.game.config.height - 5,
+      `entrance`
+    );
+    this.entrance.setImmovable(true);
+    this.entrance.displayWidth += 150;
 
-    // Setting walls as immovable
-    this.walls.getChildren().forEach((wall) => {
-      wall.setImmovable(true);
-    });
+    //exit wall
+    this.exit = this.physics.add.sprite(
+      this.sys.game.config.width / 2,
+      -50,
+      `exit`
+    );
+    this.exit.setImmovable(true);
+    this.exit.displayWidth += 600;
 
-    this.balls = this.physics.add.group();
-    for (let i = 0; i < 3; i++) {
-      let ball = this.balls.create(0, 0, 'ball');
-      ball.setCollideWorldBounds(true);
-      ball.setBounce(1);
-      ball.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200));
-    }
+    // highlight Wall
+    const graphics = this.add.graphics();
+    const highlightColor = 0x0000ff;
+    const highlightAlpha = 0.5;
+    graphics.lineStyle(5, highlightColor, highlightAlpha);
+    graphics.strokeRect(
+      this.entrance.x - this.entrance.displayWidth / 2,
+      this.entrance.y - this.entrance.height / 2,
+      this.entrance.displayWidth,
+      this.entrance.height
+    );
 
-    // Invisible wall collisions
-    this.physics.add.collider(this.player, this.walls);
-    this.physics.add.collider(this.balls, this.walls);
-    this.physics.add.collider(this.player, this.balls);
+    //create drunks
+    this.createDrunks();
 
-    this.score = 0;
-    let style = { font: '20px Arial', fill: '#fff' };
-    this.scoreText = this.add.text(20, 20, 'score: ' + this.score, style);
+    //create babys
+    this.createBabys();
 
-    this.timerText = this.add.text(600, 20, '1:00', style);
-    this.initialTime = 60;
-    this.timer = this.time.addEvent({
-      delay: 1000,
-      callback: this.updateTimer,
+    /*
+    //create babys event
+    this.createBabysEvent = this.time.addEvent({
+      delay: Phaser.Math.Between(5000, 10000),
+      callback: this.createBabys,
       callbackScope: this,
       loop: true,
+      paused: false,
     });
+
+    //create drunks
+    this.createDrunksEvent = this.time.addEvent({
+      delay: Phaser.Math.Between(4000, 5000),
+      callback: this.createDrunks,
+      callbackScope: this,
+      loop: true,
+      paused: false,
+    });
+    */
+
+    // update rep image
+    this.updateRepImage();
+
+    //repeat
+    const repeatInterval = 5000;
+
+    // Player and Entrance and Exit collision
+    this.physics.add.overlap(this.player, this.entrance);
+    this.physics.add.collider(this.player, this.exit, null, null, this);
+
+    // Score
+    this.score = 0;
+    let style = { font: `30px Arial`, fill: `#000` };
+    this.scoreText = this.add.text(600, 5, `Score: ` + this.score, style);
+    this.scoreText.setDepth(10);
+
+    // Reputation
+    this.reputation = 5;
 
     this.arrow = this.input.keyboard.createCursorKeys();
 
-    // Create buttons for pausing and ending the game
+    // buttons for pausing and ending the game
     this.createButtons();
   }
 
+  // create ---------------------------------
+
+
+  // Create Drunks
+  createDrunks() {
+    this.drunks = this.physics.add.group();
+
+    const numOfDrunks = Phaser.Math.Between(3, 6);
+
+    for (let i = 0; i < numOfDrunks; i++) {
+      const xPosition = Phaser.Math.Between(50, this.sys.game.config.width - 50);
+      const yPosition = Phaser.Math.Between(100, 300);
+
+      const drunk = this.drunks.create(xPosition, yPosition, `drunk`);
+      drunk.setCollideWorldBounds(true);
+      drunk.setBounce(0.8);
+      drunk.setVelocity(Phaser.Math.Between(-100, 100), Phaser.Math.Between(-50, 50));
+
+      this.physics.add.collider(drunk, this.entrance, this.destroyDrunk, null, this);
+      this.physics.add.collider(drunk, this.exit, this.destroyDrunkExit, null, this);
+    }
+
+    this.createDrunksEvent = this.time.addEvent({
+      delay: Phaser.Math.Between(3000, 5000),
+      callback: this.createDrunks,
+      callbackScope: this,
+      paused: false,
+    });
+  }
+
+  // Create Babys
+  createBabys() {
+    this.babys = this.physics.add.group();
+
+    const numOfBabys = Phaser.Math.Between(1, 2);
+
+    for (let i = 0; i < numOfBabys; i++) {
+      const xPosition = Phaser.Math.Between(50, this.sys.game.config.width - 50);
+      const yPosition = Phaser.Math.Between(100, 300);
+
+      const baby = this.babys.create(xPosition, yPosition, `baby`);
+      baby.setCollideWorldBounds(true);
+      baby.setBounce(0.8);
+      baby.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-50, 50));
+
+      this.physics.add.collider(baby, this.entrance, this.destroyBaby, null, this);
+      this.physics.add.collider(baby, this.exit, this.destroyBabyExit, null, this);
+    }
+
+    this.createBabysEvent = this.time.addEvent({
+      delay: Phaser.Math.Between(5000, 10000),
+      callback: this.createBabys,
+      callbackScope: this,
+      paused: false,
+    });
+  }
+
+  updateRepImage() {
+    switch (this.reputation) {
+      case 5:
+        this.repImage.setTexture(`rep5`);
+        break;
+      case 4:
+        this.repImage.setTexture(`rep4`);
+        break;
+      case 3:
+        this.repImage.setTexture(`rep3`);
+        break;
+      case 2:
+        this.repImage.setTexture(`rep2`);
+        break;
+      case 1:
+        this.repImage.setTexture(`rep1`);
+        break;
+      case 0:
+        this.repImage.setTexture(`rep0`);
+    }
+  }
+
+  // Create Buttons
   createButtons() {
-    // Button styles
     const buttonStyle = {
-      font: '24px Arial',
-      fill: '#fff',
-      backgroundColor: '#333333', // Dark gray background color
+      font: `24px Arial`,
+      fill: `#fff`,
+      backgroundColor: `#333333`,
       padding: {
         x: 10,
         y: 5,
       },
-      borderRadius: 10, // Rounded corners
+      borderRadius: 10,
     };
 
     // Pause button
     this.pauseButton = this.add.text(
       this.sys.game.config.width - 540,
-      10,
-      'Pause',
+      5,
+      `Pause`,
       buttonStyle
     );
     this.pauseButton.setInteractive();
-    this.pauseButton.on('pointerdown', this.pauseGame, this);
+    this.pauseButton.on(`pointerdown`, this.pauseGame, this);
 
     // Resume button (hidden initially)
     this.resumeButton = this.add.text(
       this.sys.game.config.width - 540,
-      10,
-      'Resume',
+      5,
+      `Resume`,
       buttonStyle
     );
     this.resumeButton.setInteractive();
     this.resumeButton.visible = false;
-    this.resumeButton.on('pointerdown', this.resumeGame, this);
+    this.resumeButton.on(`pointerdown`, this.resumeGame, this);
 
     // End button
     this.endButton = this.add.text(
       this.sys.game.config.width - 400,
-      10,
-      'End Game',
+      5,
+      `End Game`,
       buttonStyle
     );
     this.endButton.setInteractive();
-    this.endButton.on('pointerdown', this.gameOver, this);
+    this.endButton.on(`pointerdown`, this.gameOver, this);
   }
 
   pauseGame() {
     this.physics.pause();
-    this.timer.paused = true;
-    this.arrow.enabled = false; // Disable arrow key input
+    this.arrow.enabled = false;
     this.player.setVelocity(0, 0);
-
     this.pauseButton.visible = false;
     this.resumeButton.visible = true;
+
+    this.createBabysEvent.paused = true;
+    this.createDrunksEvent.paused = true;
   }
 
   resumeGame() {
     this.physics.resume();
-    this.timer.paused = false;
-    this.arrow.enabled = true; // Enable arrow key input
-
+    this.arrow.enabled = true;
     this.pauseButton.visible = true;
     this.resumeButton.visible = false;
+
+    this.createBabysEvent.paused = false;
+    this.createDrunksEvent.paused = false;
   }
 
-  update() {
-    this.physics.world.wrap(this.balls, 16);
 
-    this.balls.children.iterate((ball) => {
-      this.physics.add.collider(this.player, ball, this.hit, null, this);
-      this.physics.world.wrap(ball, 16, false, true, false, false);
+  // update -----------------------
+
+  update() {
+    this.physics.world.wrap(this.drunks, 16);
+
+    this.drunks.children.iterate((drunk) => {
+      this.physics.add.collider(this.player, drunk, this.hit, null, this);
+      this.physics.world.wrap(drunk, 16, false, true, false, false);
     });
 
+    this.babys.children.iterate((baby) => {
+      this.physics.add.collider(this.player, baby, this.hit, null, this);
+      this.physics.world.wrap(baby, 16, false, true, false, false);
+    });
+
+    // Check for hitting the walls (including the entrance wall and exit wall)
+    this.physics.world.collide(this.drunks, this.entrance);
+    this.physics.world.collide(this.babys, this.entrance);
+    this.physics.world.collide(this.drunks, this.exit, this.destroyDrunkExit, null, this);
+    this.physics.world.collide(this.babys, this.exit, this.destroyBabyExit, null, this);
+
     if (this.arrow.right.isDown) {
-      this.player.x += 5;
+      this.player.setVelocityX(350);
     } else if (this.arrow.left.isDown) {
-      this.player.x -= 5;
+      this.player.setVelocityX(-350);
+    } else {
+      this.player.setVelocityX(0);
     }
 
     if (this.arrow.down.isDown) {
-      this.player.y += 5;
+      this.player.setVelocityY(350);
     } else if (this.arrow.up.isDown) {
-      this.player.y -= 5;
-    }
-
-    this.player.setVelocity(0, 0);
-  }
-
-  updateTimer() {
-    this.initialTime--;
-    let minutes = Math.floor(this.initialTime / 60).toString().padStart(2, '0');
-    let seconds = (this.initialTime % 60).toString().padStart(2, '0');
-    this.timerText.setText(minutes + ':' + seconds);
-
-    if (this.initialTime <= 0) {
-      this.timer.paused = true;
-      this.gameOver();
+      this.player.setVelocityY(-350);
+    } else {
+      this.player.setVelocityY(0);
     }
   }
 
-  hit(player, ball) {
-    const angle = Phaser.Math.Angle.Between(player.x, player.y, ball.x, ball.y);
-    const velocityMagnitude = 500;
-    ball.setVelocity(velocityMagnitude * Math.cos(angle), velocityMagnitude * Math.sin(angle));
+  // update -----------------------
+
+
+  hit(player, sprite) {
+    const angle = Phaser.Math.Angle.Between(player.x, player.y, sprite.x, sprite.y);
+    const velocityMagnitude = 400;
+    sprite.setVelocity(velocityMagnitude * Math.cos(angle), velocityMagnitude * Math.sin(angle));
   }
+
+  // Destroy drunk
+  destroyDrunk(drunk, entrance) {
+    if (entrance === this.entrance) {
+      drunk.destroy();
+      this.score += 125;
+      this.scoreText.setText(`Score: ` + this.score);
+    }
+  }
+
+  // Destroy drunk exit
+  destroyDrunkExit(drunk, exit) {
+    if (exit === this.exit) {
+      drunk.destroy();
+      this.score -= 5;
+    }
+  }
+
+  // Destroy baby
+  destroyBaby(baby, entrance) {
+    if (entrance === this.entrance) {
+      baby.destroy();
+      this.score -= 10;
+      this.scoreText.setText(`Score: ` + this.score);
+
+      this.reputation--;
+      this.updateRepImage();
+      if (this.reputation === 0) {
+        this.gameOver();
+      }
+    }
+  }
+
+  // Destroy baby exit
+  destroyBabyExit(baby, exit) {
+    if (exit === this.exit) {
+      baby.destroy();
+    }
+  }
+
 
   gameOver() {
     this.physics.pause();
-    this.timerText.setText('Game Over');
+    this.scoreText.destroy();
+
+    this.createBabysEvent.remove();
+    this.createDrunksEvent.remove();
+
+    const firedGif = this.add.image(
+      this.sys.game.config.width / 2,
+      this.sys.game.config.height / 2 - 35,
+      `fired`
+    );
+    firedGif.setOrigin(0.5);
+    firedGif.setDepth(10);
     let finalScoreText = this.add.text(
       this.sys.game.config.width / 2,
-      this.sys.game.config.height / 2,
-      'Final Score: ' + this.score,
-      { font: '32px Arial', fill: '#fff', align: 'center' }
+      this.sys.game.config.height / 2 + 15,
+      `Final Score: ` + this.score,
+      { font: `40px Arial`, fill: `#fff`, align: `center` }
     );
     finalScoreText.setOrigin(0.5);
+    finalScoreText.setDepth(10);
   }
 }
 
 let config = {
   width: 800, // Width of the game in pixels
   height: 500, // Height of the game in pixels
-  backgroundColor: '#3498db', // The background color (blue)
+  backgroundColor: `#3498db`, // The background color (blue)
   scene: MainScene, // The name of the scene we created
-  physics: { default: 'arcade' }, // The physics engine to use
-  parent: 'game', // Create the game inside the <div id="game">
+  physics: { default: `arcade` }, // The physics engine to use
+  parent: `game`, // Create the game inside the <div id="game">
 };
 
 let game = new Phaser.Game(config);
